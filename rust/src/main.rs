ac5421bef274128269084d14bb300f9ab64dd66a
lderequesens <leonardo.sobarzo@protonmail.com>
2023-10-20 15:11:41 -0300

    wip: starting rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,49 @@
+use std::io;
+
+// fn transform_time_to_human() -> String {
+//     let hours = 
+// }
+
+fn transfrom_human_to_time(time: String) -> i32 {
+    let mut temp = 0;
+    let mut minutes = 0;
+    let mut hours = 0;
+    let mut seconds = 0;
+    let mut multi = 0;
+
+    for char in time.chars() {
+        // let char = String::from(char);
+        match char.as_str() {
+            b"h" => {
+                hours = temp;
+                temp = 0;
+                multi = 1;
+            }
+            String::from("m") => {
+                minutes = temp;
+                temp = 0;
+                multi = 1;
+            }
+            String::from("s") => {
+                seconds = temp;
+                break;
+            }
+            _ => {
+                temp = temp * multi + char.to_digit(10).unwrap();
+            }
+        }
+
+    }
+
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut human_time = String::new();
+    
+    match stdin.read_line(&mut human_time) {
+        Ok(_) => println!("Input: {}", human_time),
+        Err(e) => println!("Error: {}", e)
+    }
 }
